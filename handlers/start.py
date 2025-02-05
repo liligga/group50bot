3db4a50280907ad81a481778ca7b5b6d0f822539
Igor <igglike@gmail.com>
2025-02-05 16:03:16 +0300

    :sparkles:Добавил кнопки:sparkles:

--- handlers/start.py
+++ handlers/start.py
@@ -1,15 +1,30 @@
-from aiogram import Dispatcher, types
+from aiogram import Dispatcher
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 
 # @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
+async def start_handler(message: Message):
     user = message.from_user
     # user.first_name - имя пользователя, есть всегда
     # user.last_name - фамилия, может не быть
     # user.username - ник пользователя(@), может не быть
     # user.id - уникальный индетификатор пользователя, есть всегда, в виде чмсла
-    await message.answer(f"Hello, {user.first_name}")
+    kb = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Наш сайт", url="https://geeks.kg"),
+            InlineKeyboardButton(text="Наш инстаграм", url="https://geeks.kg"),
+        ],
+        [
+            InlineKeyboardButton(text="О нас", callback_data="aboutus"),
+        ]
+    ])
+    await message.answer(f"Приветствуем вас, {user.first_name}", reply_markup=kb)
 
+
+async def about_us_handler(callback: CallbackQuery):
+    # await callback.answer("О нас", show_alert=True)
+    await callback.message.answer("О нас")
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(start_handler, commands=['start'])
+    dp.register_callback_query_handler(about_us_handler, lambda c: c.data == "aboutus")
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from dotenv import dotenv_values
 import logging
 
+from bot_config import dp
 from handlers import (
     start,
     other_message,
     picture
 )
-
-token = dotenv_values(".env")["BOT_TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher(bot)
 
 
 async def main():
